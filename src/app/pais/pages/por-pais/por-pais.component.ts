6c72be4f86b88dd4042bda1fce1f839a737aaf0b
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-12 11:23:29 -0300

    V109 - Tabla paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface'; 
 
 @Component({
   selector: 'app-por-pais',
@@ -9,8 +10,9 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
 
-  termino : string = 'hola mundo';
-  hayError: boolean = false
+  termino : string = 'Chile';
+  hayError: boolean = false;
+  paises  : Country[] = [];
 
   constructor( private PaisService: PaisService) { }
 
@@ -33,14 +35,16 @@ export class PorPaisComponent {
  buscar(){
   this.hayError = false;
   console.log(this.termino);
-  
+
   this.PaisService.buscarPais(this.termino)
   .subscribe({
     next: (paises)=>{
       console.log(paises); 
+      this.paises = paises;
     },
     error: (err)=>{
       this.hayError = true;
+      this.paises   = [];
       console.log("Error!");
       console.info(err);
     }
