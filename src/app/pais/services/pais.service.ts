283566c0f3385a53c8119c755a55cf03adfbaa6b
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-13 12:23:20 -0300

    V126 - Optimizar Peticion HTTP

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { Country } from '../interfaces/pais.interface';
 
@@ -9,6 +9,19 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = "https://restcountries.com/v3.1/";
+  private apiUrlv2: string = "https://restcountries.com/v2/";
+
+  get httpParams (){
+ 
+     // Como retornar menos datos
+     return new HttpParams()
+     //?fields=name,capital,currencies  --- v es lo mismo
+           .set('fields', 'name,capital,currencies'); 
+
+    //para llamar metodo
+    //return this.http.get<Country[]>(url, {params: this.httpParams});    
+
+  }
 
   constructor( private http: HttpClient) { }
 
@@ -37,6 +50,8 @@ export class PaisService {
   }
 
   buscarRegion(region: string): Observable<Country[]>{
+
+   
 
     const url = `${this.apiUrl}/region/${region}`
 
